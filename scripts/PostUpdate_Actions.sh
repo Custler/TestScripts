16d2bd7cdcc43715418bd2ee11083433273e81b3
Custler <svt@zmail.ru>
2022-08-05 21:24:31 +0300

    Removed legacy code for C++ node, fixes and improv

--- scripts/PostUpdate_Actions.sh
+++ scripts/PostUpdate_Actions.sh
@@ -27,8 +27,13 @@ SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 source "${SCRIPT_DIR}/env.sh"
 
 #################################################################
-${SCRIPT_DIR}/next_elect_set_time.sh
+# ${SCRIPT_DIR}/next_elect_set_time.sh
 #################################################################
+# echo "Nothing to do."
+# Distro_Name="$(cat /etc/os-release | grep "PRETTY_NAME="|awk -F'[" ]' '{print $2}')"
+if [[ "$(uname -s)" == "FreeBSD" ]];then
+    sudo pkg install -y 7-zip
+fi
 
 echo "+++INFO: $(basename "$0") FINISHED $(date +%s) / $(date  +'%F %T %Z')"
 echo "================================================================================================"
